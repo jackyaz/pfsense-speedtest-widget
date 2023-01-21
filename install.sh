02fecc83aa6de74cb8ba0acda3128d958179b459
Jack <6189420+jackyaz@users.noreply.github.com>
2023-01-21 10:23:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 sudo pkg update && sudo pkg install -g libidn2 ca_root_nss
 # Example how to remove conflicting or old versions using pkg
 # sudo pkg remove speedtest
-sudo pkg add "https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-freebsd.pkg"
-fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/rudical/pfsense-speedtest-widget/master/speedtest.widget.php
+sudo pkg add "https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-freebsd12-x86_64.pkg"
+fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/jackyaz/pfsense-speedtest-widget/master/speedtest.widget.php
